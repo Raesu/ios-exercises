2fbfcf7c94ac41c9499766da16064a72fb4ba868
Ryan Summe <ryan.summe@gmail.com>
2015-06-09 10:10:25 +0800

    Assignment Looping

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,22 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    
+    NSMutableString *newString = [@"" mutableCopy];
+
+    NSInteger lower = number;
+    NSInteger upper = otherNumber;
+    
+    if (number > otherNumber) {
+        lower = otherNumber;
+        upper = number;
+    }
+    
+    for (NSInteger i = lower; i <= upper; i++) {
+        [newString appendFormat:@"%ld", (long)i];
+    }
+    
+    return newString;
 }
 
 @end
